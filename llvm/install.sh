670c216f1d8be2cf22c2ab0e536f466219d59447
Povilas Balciunas <balciunas90@gmail.com>
2014-11-25 10:52:13 +0200

    Added clang-format to llvm tool installation list.

--- llvm/install.sh
+++ llvm/install.sh
@@ -3,4 +3,4 @@
 sudo cp ./llvm.list /etc/apt/sources.list.d
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update
-sudo apt-get install clang-3.5 clang-3.5-doc lldb-3.5
+sudo apt-get install clang-3.5 clang-3.5-doc lldb-3.5 clang-format-3.5
