6b136c3c94bfd2e5eb2aa8dcd43c00fe2165be5b
Povilas Balciunas <balciunas90@gmail.com>
2015-03-06 14:03:14 +0200

    Fixed installation script.

--- awesome-wm/install.sh
+++ awesome-wm/install.sh
@@ -2,10 +2,10 @@
 
 install_dir=~/.config/awesome
 
-mkdir -p $(install_dir)
+mkdir -p $install_dir
 
-install --mode=644 rc.lua $(install_dir)/rc.lua
-install --mode=644 notify.lua $(install_dir)/notify.lua
-install --mode=644 lang.lua $(install_dir)/lang.lua
-install --mode=644 settings.lua $(install_dir)/settings.lua
-cp -Rf themes $(install_dir)
+install --mode=644 rc.lua $install_dir/rc.lua
+install --mode=644 notify.lua $install_dir/notify.lua
+install --mode=644 lang.lua $install_dir/lang.lua
+install --mode=644 settings.lua $install_dir/settings.lua
+cp -Rf themes $install_dir
