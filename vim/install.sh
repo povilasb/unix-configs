c4d853db4f678a61bd62179ac69442ec2797d5b3
Povilas Balciunas <balciunas90@gmail.com>
2015-11-27 12:39:25 +0200

    vim: fix configs installation
    If configs dir existed, new configs dir was copied inside the existing one.

--- vim/install.sh
+++ vim/install.sh
@@ -5,6 +5,9 @@ mkdir -p ${VIM_COLORS_DIR}
 cp povilasb_colors.vim ${VIM_COLORS_DIR}
 
 mkdir -p ~/.vim
+
+rm -rf ~/.vim/configs
 cp -r ./configs ~/.vim/configs
+
 cp -rf ./syntax ~/.vim/
 cp -rf ./ftplugin ~/.vim/
