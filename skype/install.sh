f512b29faa3da217b16d877692b399e0b9f28764
Povilas Balciunas <balciunas90@gmail.com>
2026-01-20 10:15:37 +0200

    Skype is dead

--- skype/install.sh
+++
@@ -1,7 +0,0 @@
-sudo dpkg --add-architecture i386
-sudo apt-get update
-
-SKYPE_DEB=skype-debian_4.2.0.11-1_i386.deb
-cd /tmp ; wget http://download.skype.com/linux/${SKYPE_DEB}
-sudo dpkg -i /tmp/${SKYPE_DEB}
-sudo apt-get install -f
